89ff4a2c54d07db6f8150614a6e40f8beecd9ba5
manlikevee <odahviktor@gmail.com>
2022-10-09 16:12:36 +0100

    adadbdbdjsjs

--- scrr.js
+++ scrr.js
@@ -1,30 +1,28 @@
-const form = document.querySelector("form"),
-statusTxt = form.querySelector(".button-area span");
+const btn = document.getElementById("button");
 
-form.onsubmit = (e)=>{
-  e.preventDefault();
-  statusTxt.style.color = "#0D6EFD";
-  statusTxt.style.display = "block";
-  statusTxt.innerText = "Sending your message...";
-  form.classList.add("disabled");
+document.getElementById("form").addEventListener("submit", function (event) {
+  event.preventDefault();
 
-  let xhr = new XMLHttpRequest();
-  xhr.open("POST", "action.php", true);
-  xhr.onload = ()=>{
-    if(xhr.readyState == 4 && xhr.status == 200){
-      let response = xhr.response;
-      if(response.indexOf("Email and message field is required!") != -1 || response.indexOf("Enter a valid email address!") != -1 || response.indexOf("Sorry, failed to send your message!") != -1){
-        statusTxt.style.color = "red";
-      }else{
-        form.reset();
-        setTimeout(()=>{
-          statusTxt.style.display = "none";
-        }, 3000);
-      }
-      statusTxt.innerText = response;
-      form.classList.remove("disabled");
+  btn.value = "Sending...";
+
+  const serviceID = "service_o7o1er8";
+  const templateID = "template_2u6234n";
+
+  emailjs.sendForm(serviceID, templateID, this).then(
+    () => {
+      btn.value = "Send Email";
+      alert("Thank you for contacting integrated software services limited, we would be in touch shortly");
+      document.querySelector("#from_name").value = "";
+      document.querySelector("#last_name").value = "";
+      document.querySelector("#company_name").value = "";
+      document.querySelector("#company_email").value = "";
+      document.querySelector("#job_title").value = "";
+      document.querySelector("#business_type").value = "";
+      document.querySelector("#message").value = "";
+    },
+    (err) => {
+      btn.value = "Send Email";
+      alert(JSON.stringify(err));
     }
-  }
-  let formData = new FormData(form);
-  xhr.send(formData);
-}
+  );
+});
